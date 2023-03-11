8aa5372bda65f00d7dede8b0567c656ac863426f
Buckie-Pal <vincentmooney5@gmail.com>
2023-03-10 23:20:59 -0500

    CAT

--- Assets/Scripts/cat.gd
+++ Assets/Scripts/cat.gd
@@ -1,22 +1,47 @@
 extends CharacterBody2D
 
-var hspd = 0;
-var vspd = 0;
+var hSpd = 0;
+var vSpd = 0;
 var pSpd = 2;
+var pDir = 2;
+@onready var animPlayer = $AnimationPlayer;
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	hspd = Input.get_axis("Move Left","Move Right");
-	vspd = Input.get_axis("Move Up","Move Down");
-	velocity = Vector2(hspd, vspd).normalized() * pSpd * 50;
+	hSpd = Input.get_axis("Move Left","Move Right");
+	vSpd = Input.get_axis("Move Up","Move Down");
+	velocity = Vector2(hSpd, vSpd).normalized() * pSpd * 50;
 	move_and_slide();
-
-	if velocity.x > 0:
-		$AnimatedSprite2D.scale.x = 1
-	elif velocity.x < 0:
-		$AnimatedSprite2D.scale.x = -1
-
 
 
 func _process(delta):
-	pass
+	if velocity.x < 0:
+		$Sprite2D.scale.x = -1;
+		pDir = 0;
+	elif velocity.x > 0:
+		$Sprite2D.scale.x = 1;	
+		pDir = 0;
+	
+	if velocity.y < 0:
+		pDir = 1;
+	elif velocity.y > 0:
+		pDir = 2;
+		
+	if velocity == Vector2.ZERO:
+		match pDir:
+			0:
+				animPlayer.play("cat_idle_side");
+			1:
+				animPlayer.play("cat_idle_up");
+			2:
+				animPlayer.play("cat_idle_down");
+	else:
+		match pDir:
+			0:
+				animPlayer.play("cat_walk_side");
+			1:
+				animPlayer.play("cat_walk_up");
+			2:
+				animPlayer.play("cat_walk_down");
