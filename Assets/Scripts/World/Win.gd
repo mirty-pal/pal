aefe8293fc50d5adaad34f9d2aa6717d626cdd94
mirty <127162929+mirty-pal@users.noreply.github.com>
2023-03-15 10:50:53 -0700

    weiner

--- Assets/Scripts/World/Win.gd
+++ Assets/Scripts/World/Win.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file("res://Battle System/Battlebase.tscn")
+	get_tree().change_scene_to_file("res://Battle System/Battlebase.tscn");
