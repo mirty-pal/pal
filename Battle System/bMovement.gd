6f910636749dcf5d3545f9291e686f19c867ac9b
mirty <127162929+mirty-pal@users.noreply.github.com>
2023-03-15 16:34:13 -0700

    Experimenting with something, suckin yo nutz

--- Battle System/bMovement.gd
+++ Battle System/bMovement.gd
@@ -1,30 +1,31 @@
 extends CharacterBody2D
 
 var target = Vector2.ZERO;
-var movX = 0;
-var movY = 0;
-func _physics_process(_delta):
-	if(position == target):
-		movX = int(Input.is_action_just_pressed("Move Right")) - int(Input.is_action_just_pressed("Move Left"))
-		movY = int(Input.is_action_just_pressed("Move Down")) - int(Input.is_action_just_pressed("Move Up"))
-	else:
-		movX = 0;
-		movY = 0;
+var hspd = 0;
+var vspd = 0;
+var pSpd = 2;
+var spriteArr = [["ness_ul","ness_l","ness_dl"],["ness_u","ness_forward","ness_forward"],["ness_ur","ness_r","ness_dr"]];
+var shiftKey = false;
+var Targetpos;
+func _ready():
+	print(spriteArr[1][1]);
+	$AnimationPlayer.speed_scale = 2;
+	Targetpos = get_node("/root/Node2D/looktest");
+func _physics_process(delta):
+	hspd = Input.get_axis("Move Left","Move Right");
+	vspd = Input.get_axis("Move Up","Move Down");
+	shiftKey = Input.is_key_pressed(KEY_SHIFT);
+	velocity = Vector2(hspd, vspd).normalized()*pSpd;
+	move_and_collide(velocity * 50 * delta);
 	
-	#set target and move dude towards it
-	if(movX != 0 || movY != 0):
-		target = Vector2(position.x+(42*movX),position.y+(42*movY));
-		velocity.x = sign(movX);
-		velocity.y = sign(movY);
-		velocity = 6*velocity.normalized();
-		
-		
-	#approximate float numbers 
-	var pX = abs((target.x - position.x)/position.x);
-	var pY = abs((target.y - position.y)/position.y);
-	print("pX: " + str(pX));
-	print("pY: " + str(pY));
-	
-	if(position == target):
-		velocity = Vector2(0,0);
-	move_and_collide(velocity);
+	#Triangle math:
+	print(Targetpos.position.x)
+	var xDiff = position.x - Targetpos.position.x;
+	var yDiff = position.y - Targetpos.position.y;
+	print("xDiff: " + str(xDiff))
+	print("yDiff: " + str(yDiff))
+	if(!shiftKey):
+		if(velocity != Vector2.ZERO):
+			$AnimationPlayer.play(spriteArr[hspd+1][vspd+1]);
+		else:
+			$AnimationPlayer.seek(1,true)
